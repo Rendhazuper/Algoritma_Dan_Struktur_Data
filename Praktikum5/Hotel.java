0497c98ca4deef32016ca39f1dc4e91a623ee56d
Rendha <rendhazuper@gmail.com>
2024-04-28 23:45:52 +0700

    tambahan praktikum5

---
+++ Praktikum5/Hotel.java
@@ -0,0 +1,22 @@
+package Praktikum.Praktikum5;
+
+public class Hotel {
+    public String nama, kota;
+    public int harga;
+    public byte bintang;
+
+    Hotel(String n, String k, int h, byte b) {
+        nama = n;
+        kota = k;
+        harga = h;
+        bintang = b;
+    }
+
+    void spill() {
+        System.out.println("Hotel       : " + nama);
+        System.out.println("Lokasi      : " + kota);
+        System.out.println("Price       : " + harga);
+        System.out.println("Rating      : " + bintang);
+    }
+
+}
